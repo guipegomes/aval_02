5c145d9d9f4b9476903306d5fbf3bb2f6796ca5b
Guilherme Gomes <guilherme.gomes@mediacentre.network>
2025-05-23 12:25:01 -0300

    🚧(WIP): read screen

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,6 +1,25 @@
+import { useParams, Link } from "react-router-dom";
+import { useContext } from "react";
+import { MovieContext } from "../context/MovieContext";
+
 function Read() {
+	const { id } = useParams();
+  const { movies } = useContext(MovieContext);
+
+  const movie = movies.find((m) => m.id === id);
+
+  if (!movie) {
+    return <div>Filme não encontrado</div>;
+  }
+
 	return (
-		<p>Read works!</p>
+		<div>
+      <h2>Detalhes do Filme</h2>
+      <p><strong>Title:</strong> {movie.name}</p>
+      <p><strong>Genre:</strong> {movie.genre}</p>
+      <p><strong>Release year:</strong> {movie.year}</p>
+      <Link to="/">Back</Link>
+    </div>
 	)
 }
 
